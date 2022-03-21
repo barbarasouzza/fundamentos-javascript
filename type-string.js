1d9201d5296e43637474d1abc8e09346e492dedc
Bárbara Souza <barbarasoouzza@gmail.com>
2022-03-21 17:21:02 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá Mundo!";
+const texto2 = "Olá Mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Barbara";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// template string ou templade literal
